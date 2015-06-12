6b5e972188440c6aac723f15805465c4c08f16af
jiakailian <jiakai.lian@gmail.com>
2015-06-13 08:52:25 +1000

    create the project

---
+++ IOP/IOP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IOP
+//
+//  Created by jiakai lian on 13/06/2015.
+//  Copyright (c) 2015 jiakai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
